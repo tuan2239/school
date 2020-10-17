d0c482a270f049cac5dc10b115030f218f667e4d
tuan2239 <anhtuan2231999@gmail.com>
2020-10-17 18:19:11 +0700

    s

--- projects/school-fe/src/app/views/views-routing.module.ts
+++ projects/school-fe/src/app/views/views-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ChildrenComponent } from './children/children.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ImportComponent } from './import/import.component';
 
 
 const routes: Routes = [
   { path: 'Children', component: ChildrenComponent },
   { path: '', component: DashboardComponent },
-  { path: 'Importketquathi', component: ImportComponent }
 ];
 
 @NgModule({
